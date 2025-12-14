5d6da9213685b2147b9d34ffe8f70b07d8c5d425
gagayhhad-tech <gagayhhad@gmail.com>
2025-12-15 02:29:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var burgerButton = document.getElementById('burgerMenu');
+var menu = document.getElementById('mobileMenu');
+
+burgerButton.onclick = function() {
+    burgerButton.classList.toggle('active');
+    menu.classList.toggle('active');
+};
+
+var menuLinks = document.querySelectorAll('.mobile-nav-link');
+
+for (var i = 0; i < menuLinks.length; i++) {
+    menuLinks[i].onclick = function() {
+        burgerButton.classList.remove('active');
+        menu.classList.remove('active');
+    };
+}
+
